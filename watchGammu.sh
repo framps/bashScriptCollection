109121d513350f7b9fd52dabc4f00a67ca7af78f
framp <framp@linux-tips-and-tricks.de>
2020-01-15 19:00:34 +0100

    Added eMail relay function

--- watchGammu.sh
+++ watchGammu.sh
@@ -2,7 +2,7 @@
 #######################################################################################################################
 #
 # 	Sample code for a SMS relay server with gammu
-#   Script listens for SMS received and forwards them to another phone number
+#   Script listens for SMS received and forwards them to another phone number or sends it to an eMail or does some other stuff with the SMS
 #
 #	This script is referred by https://www.linux-tips-and-tricks.de/en/raspberry/559-use-zte-ml190-usb-pen-drive-to-create-a-sms-relay-server
 #	which explains in detail how to create a SMS relay server
@@ -26,18 +26,21 @@
 #
 #######################################################################################################################
 
-GAMMU_INPUT_DIR="/var/spool/gammu/inbox"
-TARGET_PHONE="+4917147114711"
+GAMMU_INPUT_DIR="/var/spool/gammu/inbox" # directory gammu stores the received SMS
+SOURCE_PHONE="+4917147124712"	# phone number of SMS receiver
+TARGET_PHONE="+4917147114711"	# phone number of SMS relay target
+EMAIL="smsrelay@mydomain.de"	# email the SMS should be relayed to
+
 MYSELF=${0##*/}
 MYNAME=${MYSELF%.*}
 
-if [[ $1 == "start" ]]; then
+if [[ $1 == "start" ]]; then	# start sms relay
         $MYSELF >> /var/log/$MYNAME.log &
 
-elif [[ $1 == "stop" ]]; then
+elif [[ $1 == "stop" ]]; then	# stop sms relay
         killall $MYSELF
 
-else
+else	# process sms
         inotifywait -m $GAMMU_INPUT_DIR -e create | while read path action file; do
           echo "The file '$file' appeared in directory '$path' via '$action'"
           msg="$(<$path/$file)"
@@ -47,8 +50,10 @@ else
                 systemctl stop gammu-smsd
                 exit 0
           fi
-          src_phone_number="$(cut -d _ -f 4 <<< "$file")"
-          echo "$src_phone_number $msg" | gammu-smsd-inject TEXT $TARGET_PHONE
+          src_phone_number="$(cut -d _ -f 4 <<< "$file")"	# source phone number of SMS
+
+          echo "$src_phone_number $msg" | gammu-smsd-inject TEXT $TARGET_PHONE	# relay received SMS
+          cat $path/$file | mail -s "$SOURCE_PHONE: SMS received from $src_phone_number" $EMAIL # send SMS to an eMail
       done
 fi
 
