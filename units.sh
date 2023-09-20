51e565c616687b1a1d5998bcb5608e4473401b53
framps <framp@linux-tips-and-tricks.de>
2023-09-20 21:31:14 +0200

    Use (( )) op now and added P (Peta)

--- units.sh
+++ units.sh
@@ -6,7 +6,7 @@
 #
 #######################################################################################################################
 #
-#    Copyright (C) 2016 framp at linux-tips-and-tricks dot de
+#    Copyright (C) 2016,2023 framp at linux-tips-and-tricks dot de
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -25,14 +25,14 @@
 
 function convertToMetric() { # number
 
-	local DIM=(" " K M G T)
+	local DIM=(" " K M G T P)
 	local r=$1
 
-	while [[ $r -ge 1024 ]]; do
-		let r=$r/1024
-		let m=m+1
-		if [[ $m -ge ${#DIM[@]} ]]; then
-			echo "??? Number $(( $1 )) too big"
+	while (( $r >= 1024 )); do
+		(( r=$r/1024 ))
+		(( m=m+1 ))
+		if (( $m >= ${#DIM[@]} )); then
+			echo "??? Number $1 too big"
 			exit 127
 		fi
 	done
@@ -40,11 +40,11 @@ function convertToMetric() { # number
 
 }
 
-if [ "$#" -eq 1 ]; then
+if (( "$#" == 1 )) ; then
 	echo $(convertToMetric $1)
 else
-	for n in 10 1024 1024*1024+1 1024*1024*1024+1 1024*1024*1024*1024+1; do
-		echo "Number: $n - $(( $n )) - metric: $(convertToMetric $n)"
+	for n in 10 1024 1024*1024+1 1024*1024*1024+1 1024*1024*1024*1024+1 1024*1024*1024*1024*1024+1 1024*1024*1024*1024*1024*1024+1; do
+		echo "Number: $(( $n )) - metric: $(convertToMetric $(($n)))"
 	done
 fi
 
