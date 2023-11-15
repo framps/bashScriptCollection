086b9719eb06144590830663ff6efc8423df31d4
framp <framp@linux-tips-and-tricks.de>
2023-11-15 16:17:27 +0100

    Exit with return code 1 in case of error

--- backupRestoreTestWebhostingSite.sh
+++ backupRestoreTestWebhostingSite.sh
@@ -2,7 +2,7 @@
 #
 #######################################################################################################################
 #
-# 	 Create a local backup of a Webhosting website and restore the backup to a restore test website 
+# 	 Create a local backup of a Webhosting website and restore the backup to a restore test website
 #
 #	Steps:
 #	1) Create mysqldump of database
@@ -200,7 +200,7 @@ function cleanup() {
 if [[ ! -d $BACKUP_DIR/$DIRNAME$NO ]]; then
 	writeToConsole "Creating $BACKUP_DIR/$DIRNAME$NO"
 	mkdir $BACKUP_DIR/$DIRNAME$NO
-	(( $? )) && ( echo "Error creating directory $BACKUP_DIR/$DIRNAME$NO"; exit )
+	(( $? )) && { echo "Error creating directory $BACKUP_DIR/$DIRNAME$NO"; exit 1; }
 fi
 trap "cleanup" SIGINT SIGTERM EXIT
 
@@ -209,14 +209,14 @@ START_TIME=$(timerStart)
 RUN_TIME=$(timerStart)
 writeToConsole "Dumping DB $DB_BACKUPSOURCE_DBNAME to $BACKUP_DIR/$DIRNAME$NO"
 mysqldump -p $DB_BACKUPSOURCE_DBNAME -u $DB_BACKUPSOURCE_USERID -p$DB_BACKUPSOURCE_PASSWORD -h $DB_BACKUPSOURCE_SERVER --default-character-set=utf8mb4 > $BACKUP_DIR/$DIRNAME$NO/$DB_BACKUPSOURCE_DBNAME.sql
-(( failure=failure || $? )) && ( writeToConsole "DB dump of $DB_BACKUPSOURCE_DBNAME failed"; exit )
+(( failure=failure || $? )) && { writeToConsole "DB dump of $DB_BACKUPSOURCE_DBNAME failed"; exit 1; }
 RUN_TIME="$(timerEnd $RUN_TIME)"
 writeToConsole "DB Dumptime: $RUN_TIME"
 
 RUN_TIME=$(timerStart)
 writeToConsole "Restoring DB $DB_RESTORESOURCE_DBNAME from $BACKUP_DIR/$DIRNAME$NO"
 mysql -p $DB_RESTORESOURCE_DBNAME -u $DB_RESTORESOURCE_USERID -p$DB_RESTORESOURCE_PASSWORD -h $DB_RESTORESOURCE_SERVER < $BACKUP_DIR/$DIRNAME$NO/$DB_BACKUPSOURCE_DBNAME.sql
-(( failure=failure || $? )) && ( writeToConsole "Restor of DB dump to $DB_RESTORESOURCE_DBNAME failed"; exit )
+(( failure=failure || $? )) && { writeToConsole "Restor of DB dump to $DB_RESTORESOURCE_DBNAME failed"; exit 1; }
 RUN_TIME="$(timerEnd $RUN_TIME)"
 writeToConsole "DB importtime: $RUN_TIME"
 
@@ -227,7 +227,7 @@ if (( $CLONE )); then
 	else
 		REMOTE_WAS_MOUNTED=1
 	fi
-	(( $? )) && ( writeToConsole "Error mounting REMOTE_MP"; exit )
+	(( $? )) && { writeToConsole "Error mounting REMOTE_MP"; exit 1 }
 
 	RUN_TIME=$(timerStart)
 	writeToConsole "rsync remote website from $REMOTE_MP/$FS_BACKUP to $BACKUP_DIR/$DIRNAME$NO"
@@ -241,7 +241,7 @@ if (( $CLONE )); then
 
 	if [[ ! -d $REMOTE_MP/$FS_RESTORE ]]; then
 		mkdir $REMOTE_MP/$FS_RESTORE
-		(( $? )) && ( writeToConsole "Error creating directory $REMOTE_MP/$FS_RESTORE"; exit )
+		(( $? )) && { writeToConsole "Error creating directory $REMOTE_MP/$FS_RESTORE"; exit 1; }
 	fi
 
 	RUN_TIME=$(timerStart)
@@ -254,15 +254,15 @@ if (( $CLONE )); then
 	RUN_TIME=$(timerStart)
 	writeToConsole "Updating $CONFIGURATION_FILE"
 	sed -i "s/public \$host =.*;/public \$host = '$DB_RESTORESOURCE_SERVER';/" $REMOTE_MP/$FS_RESTORE/$CONFIGURATION_FILE
-	(( failure=failure || $? )) && ( writeToConsole "Error updating host"; exit 1 )
+	(( failure=failure || $? )) && { writeToConsole "Error updating host"; exit 1; }
 	sed -i "s/public \$user =.*;/public \$user = $DB_RESTORESOURCE_USERID;/" $REMOTE_MP/$FS_RESTORE/$CONFIGURATION_FILE
-	(( failure=failure || $? )) && ( writeToConsole "Error updating user"; exit 1 )
+	(( failure=failure || $? )) && { writeToConsole "Error updating user"; exit 1; }
 	sed -i "s/public \$password =.*;/public \$password = $DB_RESTORESOURCE_PASSWORD;/" $REMOTE_MP/$FS_RESTORE/$CONFIGURATION_FILE
-	(( failure=failure || $? )) && ( writeToConsole "Error updating password"; exit 1 )
+	(( failure=failure || $? )) && { writeToConsole "Error updating password"; exit 1; }
 	sed -i "s/public \$db =.*;/public \$db = $DB_RESTORESOURCE_DBNAME;/" $REMOTE_MP/$FS_RESTORE/$CONFIGURATION_FILE
-	(( failure=failure || $? )) && ( writeToConsole "Error updating db"; exit 1 )
+	(( failure=failure || $? )) && { writeToConsole "Error updating db"; exit 1; }
 	sed -i "s@public \$live_site =.*;@public \$live_site = '$BACKUP_URL';@" $REMOTE_MP/$FS_RESTORE/$CONFIGURATION_FILE
-	(( failure=failure || $? )) && ( writeToConsole "Error updating live_site"; exit 1 )
+	(( failure=failure || $? )) && { writeToConsole "Error updating live_site"; exit 1; }
 	RUN_TIME="$(timerEnd $RUN_TIME)"
 	writeToConsole "Website config update time: $RUN_TIME"
 
